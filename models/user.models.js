7b9242132375251859d09d7e1417ca21c92cc1fc
Aleksanrda <kryvkoalexandra05@gmail.com>
2023-02-23 15:57:42 +0000

    fix patchUserPreferences and change Promise to throw

--- models/user.models.js
+++ models/user.models.js
@@ -22,13 +22,34 @@ async function fetchUserByUserId(user_id) {
 async function patchUserPreferences(user_id, preferences) {
     //check user exists in database
     await fetchUserByUserId(user_id);
-  
-    const sqlPatchQuery = `UPDATE users SET preferences = $1 WHERE uid = $2`;
-  
+
+    // check preferences exists
+    if (!preferences) {
+      throw { status: 400, msg: "Invalid request: Missing preferences" };
+    }
+
+    // check preferences is string
+    if (typeof preferences !== "string") {
+      throw { status: 400, msg: "Invalid request: preferences is not a string" };
+    }
+
+    // check preferences string length >= 4
+    if (preferences.length < 4) {
+      throw {
+        status: 400,
+        msg: "Invalid preferences: String length must be greater than 3",
+      };
+    }
+
+    //returns user object
     try {
-      await db.query(sqlPatchQuery, [preferences, user_id]);
-  
-      return "User preferences updated";
+      const sqlPatchQuery = `UPDATE users SET preferences = $1 WHERE uid = $2 RETURNING *;`;
+
+      const {
+        rows: [user],
+      } = await db.query(sqlPatchQuery, [preferences, user_id]);
+
+      return user;
     } catch (error) {
       throw { status: 500, msg: error.message };
     }
